e3fd809a23daa7ec741289bd20363ccb8594e72e
nvalidov <niyazvalidov@gmail.com>
2018-11-20 14:42:57 +0300

    access control

---
+++ contracts/PaintersAccessControl.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.24;
+
+contract PaintersAccessControl {
+    address public ceoAddress;
+
+    bool public paused = false;
+
+    modifier onlyCEO() {
+        require(msg.sender == ceoAddress);
+        _;
+    }
+
+    function setCEO(address _newCEO) external onlyCEO {
+        require(_newCEO != address(0));
+
+        ceoAddress = _newCEO;
+    }
+
+    modifier whenNotPaused() {
+        require(!paused);
+        _;
+    }
+
+    modifier whenPaused {
+        require(paused);
+        _;
+    }
+
+    function pause() external onlyCEO whenNotPaused {
+        paused = true;
+    }
+
+    function unpause() public onlyCEO whenPaused {
+        paused = false;
+    }
+}
