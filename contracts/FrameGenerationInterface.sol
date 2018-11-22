ac066451baef529a5794922077a1364d8d0204f4
nvalidov <niyazvalidov@gmail.com>
2018-11-22 17:46:13 +0300

    interface fix

--- contracts/FrameGenerationInterface.sol
+++ contracts/FrameGenerationInterface.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.4.24;
 contract FrameGenerationInterface {
     function isFrameGenerationInterface() public pure returns (bool);
 
-    function genFrame(address creator, uint256 hash, uint256 selectedFrame, uint256 selectedFrameType)
+    function genFrame(address creator, uint256 hash, uint256 paidFrameId, uint256 selectedFrameType)
     public returns (bool framePaid, uint256 frameSeed, uint256 balance);
 }
