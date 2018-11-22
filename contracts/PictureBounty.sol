23dd54401f5143db7509b846491b166c4b56aceb
nvalidov <niyazvalidov@gmail.com>
2018-11-22 17:59:18 +0300

    public ownerShipTokenCount

---
+++ contracts/PictureBounty.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+import "./PictureOwnerShip.sol";
+
+contract PictureBounty is PictureOwnerShip {
+    uint public constant BOUNTY_FRAMES_LIMIT = 5000;
+    uint public constant BOUNTY_CANVASES_LIMIT = 1000000;
+
+    mapping
+}
