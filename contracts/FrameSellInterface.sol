6a80ef2ecf1e7d340bb06b41135d3cf622f05953
nvalidov <niyazvalidov@gmail.com>
2018-12-07 15:09:41 +0300

    frameSell

--- contracts/FrameSellInterface.sol
+++ contracts/FrameSellInterface.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.24;
 
 contract FrameSellInterface {
-    function isFrameSellInterface() public pure returns (bool);
+    function isFrameSellInterface() external pure returns (bool);
 
     function getFrame(uint256 frameId, uint256 paidAmount)
     external returns (bool framePaid, uint256 frameSeed, uint256 excess);
