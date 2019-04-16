c87baee91b7fbcfee6af682c168da3caecaeba6c
nvalidov <niyazvalidov@gmail.com>
2019-04-16 19:35:39 +0300

    ping function

--- contracts/PictureCore.sol
+++ contracts/PictureCore.sol
@@ -8,10 +8,17 @@ contract PictureCore is PictureSale {
         ceoAddress = msg.sender;
     }
 
+    address public lastPingFrom;
+    uint256 public pingCost = 0;
+
+    function setPingCost(uint256 _cost) external onlyCOO {
+        pingCost = _cost;
+    }
+
     function setCanvasCost(uint256 _cost) external onlyCOO {
         canvasCost = _cost;
     }
-
+gg
     function setSaleCut(uint256 _cut) external onlyCOO {
         require(_cut <= 10000);
         saleCut = _cut;
@@ -23,6 +30,11 @@ contract PictureCore is PictureSale {
         if (balance > 0) {
             cooAddress.transfer(balance);
         }
+    }
+
+    function ping() external payable {
+        require(msg.value >= pingCost);
+        lastPingFrom = msg.sender;
     }
 
     function getPicture(uint256 _id)
